ff9ed5896eeab011dbd2e15a2b3a94fed528597d
bebebebeb1 <n.zaharii@student.neoversity.com.ua>
2026-02-13 15:07:26 +0200

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,59 @@
+import { defineConfig } from 'vite';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+import SortCss from 'postcss-sort-media-queries';
+
+export default defineConfig(({ command }) => {
+  return {
+    base: '/kyrcov.github.io/', // 🔥 ГОЛОВНЕ ВИПРАВЛЕННЯ
+
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+
+    root: 'src',
+
+    build: {
+      sourcemap: true,
+      outDir: '../dist',
+      emptyOutDir: true,
+
+      rollupOptions: {
+        input: {
+          main: 'index.html',
+          favorites: 'favorites.html',
+        },
+
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
+            }
+            return '[name].js';
+          },
+
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
+        },
+      },
+    },
+
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/*.html']),
+      SortCss({
+        sort: 'mobile-first',
+      }),
+    ],
+  };
+});
